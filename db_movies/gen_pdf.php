8b78a7c87d499e3877b7e3300c09b4ae5c6b3125
Sharipov <46562660+Shar1pov@users.noreply.github.com>
2021-12-25 21:43:11 +0500

    Страницы для выгрузки данных в PDF и XLS

---
+++ db_movies/gen_pdf.php
@@ -0,0 +1,65 @@
+<?php
+  $servername = "localhost";
+  $database = "sharipov_labs";
+  $username = "sharipov_labs";
+  $password = "pass1234";
+  // Создаем соединение
+  $conn = mysqli_connect($servername, $username, $password, $database);
+  // Проверяем соединение
+  mysqli_set_charset($conn, "utf8mb4");
+  if (!$conn) {
+    die("Невозможно подключиться к серверу: " . mysqli_connect_error());
+  }        
+
+  require('fpdf/fpdf.php');
+
+  define('FPDF_FONTPATH',"fpdf/font/");
+
+  $result= mysqli_query($conn,"SELECT * FROM filmshow INNER JOIN movie ON filmshow.id_movie=movie.id_movie INNER JOIN cinema ON filmshow.id_cinema = cinema.id_cinema");
+
+  class PDF_gen extends FPDF
+  {
+      function Table($result)
+      {   
+        
+          $this->SetFillColor(184, 218, 255);
+       
+          $this->Cell(15, 10, iconv('utf-8', 'windows-1251',"№ п/п"), 1, 0, 'L',1);
+          $this->Cell(90, 10, iconv('utf-8', 'windows-1251',"Название фильма"), 1, 0, 'L',1);
+          $this->Cell(50, 10, iconv('utf-8', 'windows-1251',"Жанр"), 1, 0, 'L',1);
+          $this->Cell(40, 10, iconv('utf-8', 'windows-1251',"Год выпуска"), 1, 0, 'L',1);
+          $this->Cell(65, 10, iconv('utf-8', 'windows-1251',"Название зала"), 1, 0, 'L',1);
+          $this->Cell(50, 10, iconv('utf-8', 'windows-1251',"Категория"), 1, 0, 'L',1);
+          $this->Cell(45, 10, iconv('utf-8', 'windows-1251',"Дата и время показа"), 1, 0, 'L',1);
+          $this->Cell(40, 10, iconv('utf-8', 'windows-1251',"Свободных мест"), 1, 0, 'L',1);
+          $this->Ln();
+          $counter_items = 1;
+          while ($row = mysqli_fetch_array($result)) {
+              $this->SetFillColor(255, 255, 255);
+              $this->Cell(15, 7, $counter_items, 1, 0, 'C',1);
+              $counter_items++;
+              $this->Cell(90, 7, iconv('utf-8', 'windows-1251',$row['movie_name']), 1, 0, 'L',1);
+              $this->Cell(50, 7, iconv('utf-8', 'windows-1251',$row['movie_genre']), 1, 0, 'L',1);
+              $this->Cell(40, 7, iconv('utf-8', 'windows-1251',$row['movie_god_vipuska']), 1, 0, 'L',1);
+              $this->Cell(65, 7, iconv('utf-8', 'windows-1251',$row['name_cinema']), 1, 0, 'L',1);
+              $this->Cell(50, 7, iconv('utf-8', 'windows-1251',$row['category_cinema']), 1, 0, 'L',1);
+              $this->Cell(45, 7, date('d.m.Y',strtotime($row['timeshow'])), 1, 0, 'L',1);
+              $this->Cell(40, 7, iconv('utf-8', 'windows-1251',$row['booked_places']), 1, 0, 'L',1);
+              $this->Ln();
+
+          }
+      }
+  }
+
+  $pdf= new PDF_gen();
+  $pdf->AddFont('Montserrat','','Montserrat.php');
+  $pdf->SetFont('Montserrat');
+  $pdf->AddPage('L','A3');
+  $pdf->SetDisplayMode('real','default');
+  $pdf->SetFontSize(10);
+  $pdf->Table ($result);
+
+  $pdf->SetAuthor('Sharipov Ilyan Rustemovich');
+  $pdf->SetTitle('Movies');
+  $pdf->Output('Movies.pdf','D');
+?>
---
+++ db_movies/gen_xls.php
@@ -0,0 +1,62 @@
+<?php
+  $servername = "localhost";
+  $database = "sharipov_labs";
+  $username = "sharipov_labs";
+  $password = "pass1234";
+  // Создаем соединение
+  $conn = mysqli_connect($servername, $username, $password, $database);
+  // Проверяем соединение
+  mysqli_set_charset($conn, "utf8mb4");
+  if (!$conn) {
+    die("Невозможно подключиться к серверу: " . mysqli_connect_error());
+  }
+
+  require_once 'PHPExcel/Classes/PHPExcel.php';
+  $newObj = new PHPExcel();
+  $sheet_item = $newObj->getActiveSheet();
+  $result = mysqli_query($conn,"SELECT * FROM filmshow INNER JOIN movie ON filmshow.id_movie=movie.id_movie INNER JOIN cinema ON filmshow.id_cinema = cinema.id_cinema");
+
+
+  $sheet_item->getColumnDimension('A')->setAutoSize(true);
+  $sheet_item->getColumnDimension('B')->setAutoSize(true) ;
+  $sheet_item->getColumnDimension('C')->setAutoSize(true) ;
+  $sheet_item->getColumnDimension('D')->setAutoSize(true) ;
+  $sheet_item->getColumnDimension('E')->setAutoSize(true) ;
+  $sheet_item->getColumnDimension('F')->setAutoSize(true) ;
+  $sheet_item->getColumnDimension('G')->setAutoSize(true) ;
+  $sheet_item->getColumnDimension('H')->setAutoSize(true) ;
+  $sheet_item->getColumnDimension('I')->setAutoSize(true);
+  $sheet_item->setCellValue('A1','№ п/п');
+  $sheet_item->setCellValue('B1','Название фильма');
+  $sheet_item->setCellValue('C1','Жанр');
+  $sheet_item->setCellValue('D1','Год выпуска');
+  $sheet_item->setCellValue('E1','Название зала');
+  $sheet_item->setCellValue('F1','Категория');
+  $sheet_item->setCellValue('G1','Дата и время показа');
+  $sheet_item->setCellValue('H1','Свободных мест');
+
+
+  $counter = 1;
+  $ct = 2;
+
+  while ($row = mysqli_fetch_array($result)) {
+      $vacant_places = $row['places'] - $row['booked_places'];
+      $sheet_item->setCellValue('A'.$ct.'',$counter);
+      $sheet_item->setCellValue('B'.$ct.'',$row['movie_name']);
+      $sheet_item->setCellValue('C'.$ct.'',$row['movie_genre']);
+      $sheet_item->setCellValue('D'.$ct.'',$row['movie_god_vipuska']);
+      $sheet_item->setCellValue('E'.$ct.'',$row['name_cinema']);
+      $sheet_item->setCellValue('F'.$ct.'',$row['category_cinema']);
+      $sheet_item->setCellValue('G'.$ct.'',date('d.m.Y',strtotime($row['timeshow'])));
+      $sheet_item->setCellValue('H'.$ct.'',$vacant_places);
+      $ct++;
+      $counter++;
+  }
+
+  header("Content-Disposition:attachment;filename=Movies.xls");
+
+  $creator = PHPExcel_IOFactory::createWriter($newObj, 'Excel5');
+  $creator->save('php://output');
+
+  exit();
+?>
